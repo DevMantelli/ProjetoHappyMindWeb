deb2d0b532b2599a8db2a97f7f5052721316b7d2
WDYitz <yitzhakrodriguez.0901@gmail.com>
2023-02-02 17:31:15 -0300

    HappyMind - 02/02/2023
    Modificações nos Slides e no CSS

---
+++ HappyMind/js/mobile-menu.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+    let linha1 = document.querySelector('.bnt--mobile-linha-1');
+    let linha2 = document.querySelector('.bnt--mobile-linha-2');
+    let linha3 = document.querySelector('.bnt--mobile-linha-3');
+    let navArea = document.querySelector('.nav--area');
+
+    if (navArea.style.width == '0%') {
+        navArea.style.width = '40%';
+        linha1.classList.add('active--linha-1');
+        linha2.classList.add('active--linha-2');
+        linha3.classList.add('active--linha-3');
+    } else {
+        navArea.style.width = '0%';
+        linha1.classList.remove('active--linha-1');
+        linha2.classList.remove('active--linha-2');
+        linha3.classList.remove('active--linha-3');
+    }
+
+
+}
